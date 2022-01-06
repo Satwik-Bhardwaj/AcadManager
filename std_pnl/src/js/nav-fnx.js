6f6a32bb7e7697a23005cf8a1b0902f583fe2b51
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2022-01-06 11:01:56 +0530

    adding student panel, under development

---
+++ std_pnl/src/js/nav-fnx.js
@@ -0,0 +1,8 @@
+var stng_btn = document.getElementById("settings");
+
+function btn_hvr_eff(x) {
+    x.style.backgroundColor = "var(--hl-opq)";
+}
+function btn_nml_eff(x) {
+    x.style.backgroundColor = "#00000000";
+}
