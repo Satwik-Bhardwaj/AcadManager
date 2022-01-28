160a5add0dec80b40c6eed72c0c1d58b795b4fd3
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2022-01-28 13:19:18 +0530

    std_pnl changes 1

--- std_pnl/src/js/nav-fnx.js
+++
@@ -1,8 +0,0 @@
-var stng_btn = document.getElementById("settings");
-
-function btn_hvr_eff(x) {
-    x.style.backgroundColor = "var(--hl-opq)";
-}
-function btn_nml_eff(x) {
-    x.style.backgroundColor = "#00000000";
-}
